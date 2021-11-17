29a438c4828885741ce956a5032544f0162f1411
Ángela Bonilla <44753691+angelaagmez@users.noreply.github.com>
2021-11-17 16:54:34 +0100

    Add files via upload

---
+++ UD02-CSS/grid/prueba.js
@@ -0,0 +1,5 @@
+let fecha = new Date();
+console.log("Fecha: " + fecha);
+console.log("toString: " + fecha.toString());
+console.log("toUTCString: " + fecha.toUTCString());
+console.log("toDateString: " + fecha.toDateString());
