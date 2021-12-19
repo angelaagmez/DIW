4bbc56fdace92c2b4c23709c6c9d474ad7676301
Ángela Bonilla <44753691+angelaagmez@users.noreply.github.com>
2021-12-19 16:59:24 +0100

    Add files via upload

---
+++ UD03 - Animaciones Web/3.2.Canvas/ej03.js
@@ -0,0 +1,87 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del canvas
+canvas.width = 300;
+canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10;
+
+//-- Velocidades del objeto
+let velx = 3;
+let vely = 1;
+let contador = 0;
+
+
+// Generar color aleatorio
+
+function aleatorio(down, up) {
+    num = up - down;
+    random = Math.random() * num;
+    random = Math.floor(random);
+    return parseInt(down) + random;
+}
+
+function colores() {
+    return "rgb(" + aleatorio(0, 255) + "," + aleatorio(0, 255) + "," + aleatorio(0, 255) + ")";
+}
+
+//-- Funcion principal de animacion
+function update() 
+{
+    console.log("test");
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+
+   //-- Condicion de rebote en extremos verticales del canvas
+    if (x < 0 || x >= (canvas.width - 20) ) {
+    velx = -velx;
+    }
+
+  //-- Condición de rebote en extremos horizontales del canvas
+    if (y <= 0 || y > canvas.height - 20) {
+    vely = -vely;
+    }
+
+  //-- Actualizar la posición
+    x = x + velx;
+    y = y + vely;
+
+  //-- 2) Borrar el canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+    ctx.beginPath();
+    ctx.rect(x, y, 20, 20);
+
+    //-- Dibujar
+    if(x < 0 || x >= (canvas.width - 20)||y <= 0 || y > (canvas.height - 20)){
+        ctx.fillStyle = colores();
+        contador++;
+
+        if(contador==3){
+            velx = 1;
+        }
+    }
+
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+    ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+    requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
